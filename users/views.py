8463a19f8f0d09f14a2238aab8350c439003415d
Adrian <chuck.d.kc@gmail.com>
2020-06-08 18:48:16 -0500

    added Login and Logout features

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 
@@ -12,7 +13,11 @@ def register(request):
 			username= form.cleaned_data.get('username')
 
 			messages.success(request, f'Account created for {username}!')#display message 
-			return redirect('blog-home')
+			return redirect('login')
 	else:
 			form = UserRegisterForm()#create new instance of form(no POST data)
 	return render(request, 'users/register.html', {'form':form})
+
+@login_required
+def profile(request):
+	return render(request, 'users/profile.html')
